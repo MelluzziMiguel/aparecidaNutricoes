d6e1dd70bcfb6ded05b9ab5445df2f012184d514
Miguel Melluzzi <110363714+MelluzziMiguel@users.noreply.github.com>
2023-09-13 08:19:33 -0300

    Add files via upload

--- js/principal.js
+++ js/principal.js
@@ -6,4 +6,24 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
-var imc = peso / (altura * altura);
+var tdimc = paciente.querySelector(".info-imc");
+
+var pesoValido = true;
+var alturaValida = true;
+
+
+if (peso <= 0 || peso >= 550){
+    console.log("Peso Inválido!");
+    pesoValido = false;
+    tdimc.textContent = "Peso Inválido";
+}
+
+if (altura <= 0 || altura >= 3.00){
+    alturaValida = false;
+    tdimc.textContent = "Altura inválida!";
+}
+
+if( alturaValida && pesoValido){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
