a8e25b18eb2beb9969c3805ab2ea66531c775f55
Miguel Melluzzi <110363714+MelluzziMiguel@users.noreply.github.com>
2023-10-18 08:20:12 -0300

    Add files via upload

--- js/principal.js
+++ js/principal.js
@@ -1,38 +1,67 @@
-var titulo = document.querySelector(".titulo");
-titulo.textContent = "Aparecida Nutricionista";
-
 var pacientes = document.querySelectorAll(".paciente");
 
-for(var i = 0; i <pacientes.length ; i++){
+for (var i = 0; i < pacientes.length; i++) {
 
     var paciente = pacientes[i];
 
     var tdPeso = paciente.querySelector(".info-peso");
     var peso = tdPeso.textContent;
-    
+
     var tdAltura = paciente.querySelector(".info-altura");
     var altura = tdAltura.textContent;
-    
+
     var tdImc = paciente.querySelector(".info-imc");
-    
-    
-    var pessoa_valida = true
-    
-    if (peso <= 0 || peso >= 400){
-        console.log("Pessoa invalida");
-        pessoa_valida = false
-        tdImc.textContent = "Pessoa invalida"
+
+    var pesoValido = true;
+    var alturaValida = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido!");
+        pesoValido = false;
+        tdImc.textContent = "Peso inválido";
+        paciente.classList.add("paciente-invalido");
     }
-    
-    if (altura <= 0 || altura >= 3.5){
-        console.log("Pessoa invalida");
-        pessoa_valida = false
-        tdImc.textContent = "Pessoa invalida"
+
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválida!");
+        alturaValida = false;
+        tdImc.textContent = "Altura inválida";
     }
-    
-    if (pessoa_valida){
+
+  if (alturaValida && pesoValido) {
         var imc = peso / (altura * altura);
-        tdImc.textContent = imc.toFixed(2); 
+        tdImc.textContent = imc.toFixed(2);
     }
 }
 
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
+    
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("Tr");
+
+    var nomeTd = document.createElement("Td");
+    var pesoTd = document.createElement("Td");
+    var alturaTd = document.createElement("Td");
+    var gorduraTd = document.createElement("Td");
+    var imcTd = document.createElement("Td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    // imcTd.textContent = imc;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+});
