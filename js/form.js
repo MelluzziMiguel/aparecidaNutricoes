725935063f67d7bf5ab404cbec41e7945d38c7ef
Miguel Melluzzi <110363714+MelluzziMiguel@users.noreply.github.com>
2023-10-20 09:56:42 -0300

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,37 @@
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
+    
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("Tr");
+
+    var nomeTd = document.createElement("Td");
+    var pesoTd = document.createElement("Td");
+    var alturaTd = document.createElement("Td");
+    var gorduraTd = document.createElement("Td");
+    var imcTd = document.createElement("Td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = calculaImc(peso, altura);
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    var tabela = document.querySelector("#tabela-pacientes")
+
+    tabela.appendChild(pacienteTr)
+
+    console.log(pacienteTr);
+});
